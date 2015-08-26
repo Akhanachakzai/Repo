b0e77ef93432375b96d53c1c1ee776f515552b09
Zirak Khan <Zirak Khan@192.168.1.6>
2015-08-27 02:41:27 +0500

    Intial checkin of the program

---
+++ src/Repository/Repo1.java
@@ -0,0 +1,15 @@
+package Repository;
+
+public class Repo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i= 10;
+		System.out.println("Production Of My First Repository");
+for(i=0;i<=10;i++){
+	System.out.println("The Value is = "+i);
+	
+}
+	}
+
+}
